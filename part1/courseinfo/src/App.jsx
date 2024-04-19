c78ea5a96d2d2dc18da3a11b47a28b6e8051e1c1
Joni Hiltunen <joni@jonihiltunen.com>
2024-04-19 16:30:31 +0300

    Exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,24 @@
 /* eslint-disable react/prop-types */
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const exercises = [exercises1, exercises2, exercises3];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={exercises} />
-      <Total exercises={exercises} />
+      <Content parts={[part1, part2, part3]} />
+      <Total parts={[part1, part2, part3]} />
     </div>
   );
 };
@@ -29,18 +34,21 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0]} exercise={props.exercises[0]} />
-      <Part part={props.parts[1]} exercise={props.exercises[1]} />
-      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
 
 const Part = (props) => {
+  const name = props.part.name;
+  const exercises = props.part.exercises;
+
   return (
     <div>
       <p>
-        {props.part} {props.exercise}
+        {name} {exercises}
       </p>
     </div>
   );
@@ -51,11 +59,12 @@ const Total = (props) => {
     <div>
       <p>
         Number of exercises{" "}
-        {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
       </p>
     </div>
   );
 };
 
 export default App;
-
